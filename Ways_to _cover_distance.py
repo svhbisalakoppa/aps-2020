189723b7ef899390ce1645af1b1e6b3e8e1bb28f
svhbisalakoppa <52064554+svhbisalakoppa@users.noreply.github.com>
2020-03-19 20:03:49 +0530

    Create Ways_to _cover_distance.py

---
+++ Ways_to _cover_distance.py
@@ -0,0 +1,26 @@
+# A naive recursive Python3 program 
+# to count number of ways to cover 
+# a distance with 1, 2 and 3 steps 
+
+# Returns count of ways to 
+# cover 'dist' 
+def printCountRec(dist): 
+	
+	# Base cases 
+	if dist < 0: 
+		return 0
+		
+	if dist == 0: 
+		return 1
+
+	# Recur for all previous 3 and	 
+# add the results 
+	return (printCountRec(dist-1) +
+			printCountRec(dist-2) +
+			printCountRec(dist-3)) 
+
+# Driver code 
+dist = 4
+print(printCountRec(dist)) 
+
+# This code is contributed by Anant Agarwal. 
