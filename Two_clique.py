3982dc0eb7e52e7c6b31084443e23cce8963c686
svhbisalakoppa <52064554+svhbisalakoppa@users.noreply.github.com>
2020-03-24 15:45:49 +0530

    Create Two_clique.py

---
+++ Two_clique.py
@@ -0,0 +1,96 @@
+# Python3 program to find out whether a given 
+# graph can be converted to two Cliques or not. 
+from queue import Queue 
+
+# This function returns true if subgraph 
+# reachable from src is Bipartite or not. 
+def isBipartiteUtil(G, src, colorArr): 
+	global V 
+	colorArr[src] = 1
+
+	# Create a queue (FIFO) of vertex numbers 
+	# and enqueue source vertex for BFS traversal 
+	q = Queue() 
+	q.put(src) 
+
+	# Run while there are vertices in 
+	# queue (Similar to BFS) 
+	while (not q.empty()): 
+		
+		# Dequeue a vertex from queue 
+		u = q.get() 
+
+		# Find all non-colored adjacent vertices 
+		for v in range(V): 
+			
+			# An edge from u to v exists and 
+			# destination v is not colored 
+			if (G[u][v] and colorArr[v] == -1): 
+				
+				# Assign alternate color to this 
+				# adjacent v of u 
+				colorArr[v] = 1 - colorArr[u] 
+				q.put(v) 
+
+			# An edge from u to v exists and destination 
+			# v is colored with same color as u 
+			elif (G[u][v] and colorArr[v] == colorArr[u]): 
+				return False
+
+	# If we reach here, then all adjacent 
+	# vertices can be colored with alternate color 
+	return True
+
+# Returns true if a Graph G[][] is Bipartite or not. 
+# Note that G may not be connected. 
+def isBipartite(G): 
+	global V 
+	
+	# Create a color array to store colors assigned 
+	# to all veritces. Vertex number is used as index 
+	# in this array. The value '-1' of colorArr[i] 
+	# is used to indicate that no color is assigned 
+	# to vertex 'i'. The value 1 is used to indicate 
+	# first color is assigned and value 0 indicates 
+	# second color is assigned. 
+	colorArr = [-1] * V 
+
+	# One by one check all not yet 
+	# colored vertices. 
+	for i in range(V): 
+		if (colorArr[i] == -1): 
+			if (isBipartiteUtil(G, i, colorArr) == False): 
+				return False
+
+	return True
+
+# Returns true if G can be divided into 
+# two Cliques, else false. 
+def canBeDividedinTwoCliques(G): 
+	global V 
+	
+	# Find complement of G[][] 
+	# All values are complemented 
+	# except diagonal ones 
+	GC = [[None] * V for i in range(V)] 
+	for i in range(V): 
+		for j in range(V): 
+			GC[i][j] = not G[i][j] if i != j else 0
+
+	# Return true if complement is 
+	# Bipartite else false. 
+	return isBipartite(GC) 
+
+# Driver Code 
+V = 5
+
+G = [[0, 1, 1, 1, 0], 
+	[1, 0, 1, 0, 0], 
+	[1, 1, 0, 0, 0], 
+	[0, 1, 0, 0, 1], 
+	[0, 0, 0, 1, 0]] 
+
+if canBeDividedinTwoCliques(G): 
+	print("Yes") 
+else: 
+	print("No") 
