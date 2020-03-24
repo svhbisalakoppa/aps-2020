59978640d2d7f8670045942d47c0ed9269a47b2b
svhbisalakoppa <52064554+svhbisalakoppa@users.noreply.github.com>
2020-03-24 12:05:10 +0530

    Create Juggler_sequence.py

---
+++ Juggler_sequence.py
@@ -0,0 +1,33 @@
+import math 
+
+#This function prints the juggler Sequence 
+def printJuggler(n) : 
+	a = n 
+	
+	# print the first term 
+	print a, 
+	
+	# calculate terms until last term is not 1 
+	while (a != 1) : 
+		b = 0
+		
+		# Check if previous term is even or odd 
+		if (a%2 == 0) : 
+			
+			# calculate next term 
+			b = (int)(math.floor(math.sqrt(a))) 
+
+		else : 
+			# for odd previous term calculate 
+			# next term 
+			b = (int) (math.floor(math.sqrt(a)*math.sqrt(a)*
+										math.sqrt(a))) 
+
+		print b, 
+		a = b 
+
+printJuggler(3) 
+print
+printJuggler(9) 
+
+# This code is contributed by Nikita Tiwari. 
